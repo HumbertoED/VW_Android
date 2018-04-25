d29303b519e47ba865affcc178be634a31cd9285
Alumno <alumno@iMac-de-ITESM-5.local>
2018-04-25 18:54:04 -0500

    Initial commit

---
+++ app/src/main/java/com/example/alumno/vwmove/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.alumno.vwmove;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText username;
+    EditText password;
+    Button login;
+    Button forgot;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        password = (EditText)findViewById(R.id.passwordTxt);
+        username = (EditText)findViewById(R.id.userTxt);
+        login = (Button)findViewById(R.id.loginTxt);
+        forgot = (Button)findViewById(R.id.forgotTxt);
+    }
+
+    public void log_in(){
+
+    }
+
+    public void forgot_pass(){
+
+
+    }
+}
