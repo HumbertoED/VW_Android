b38f90e4081b06e89581deaa263e7d489611b662
HumbertoED <a01550159@itesm.mx>
2018-04-30 16:01:37 -0500

    The counter works!

--- app/src/main/java/com/example/alumno/vwmove/RoutesFragment.java
+++ app/src/main/java/com/example/alumno/vwmove/RoutesFragment.java
@@ -10,6 +10,9 @@ import android.view.ViewGroup;
 
 
 public class RoutesFragment extends Fragment {
+
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
