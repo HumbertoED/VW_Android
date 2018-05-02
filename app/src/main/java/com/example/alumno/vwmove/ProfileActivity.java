d0ab3bac888f27d55d52bc5121bf1c87324cde12
HumbertoED <a01550159@itesm.mx>
2018-05-01 19:30:48 -0500

    works

--- app/src/main/java/com/example/alumno/vwmove/ProfileActivity.java
+++ app/src/main/java/com/example/alumno/vwmove/ProfileActivity.java
@@ -2,8 +2,14 @@ package com.example.alumno.vwmove;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    Button goPerformance;
+    Button goPassword;
+    Button goComments;
+    Button goClose;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
