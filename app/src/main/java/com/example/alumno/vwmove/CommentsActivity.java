b89c7c614af051cf536ccf829c014f69ba5703f1
PaoGv93 <Pao_Gv93@hotmail.com>
2018-05-01 23:06:41 -0500

    holi2

--- app/src/main/java/com/example/alumno/vwmove/CommentsActivity.java
+++ app/src/main/java/com/example/alumno/vwmove/CommentsActivity.java
@@ -2,12 +2,16 @@ package com.example.alumno.vwmove;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class CommentsActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_comments);
+        setContentView(R.layout.activity_main);
+
     }
 }
