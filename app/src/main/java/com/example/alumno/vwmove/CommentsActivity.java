aeb19f9c11f2ae90bff47cf87dead2b7965ed0f6
HumbertoED <a01550159@itesm.mx>
2018-05-02 00:54:51 -0500

    In your face, XD

--- app/src/main/java/com/example/alumno/vwmove/CommentsActivity.java
+++ app/src/main/java/com/example/alumno/vwmove/CommentsActivity.java
@@ -1,17 +1,39 @@
 package com.example.alumno.vwmove;
 
-import android.os.Bundle;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Properties;
+
 public class CommentsActivity extends AppCompatActivity {
+
+    Spinner spinner;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_comments);
+
+        Spinner spinner = (Spinner) findViewById(R.id.time_spinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.selection_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
 
     }
+
 }
