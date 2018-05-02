28be3407afa36fbc605d9729b82a56feb95c6bac
PaoGv93 <Pao_Gv93@hotmail.com>
2018-05-01 21:43:06 -0500

    holi2

--- app/src/main/java/com/example/alumno/vwmove/RouteActivity.java
+++ app/src/main/java/com/example/alumno/vwmove/RouteActivity.java
@@ -1,13 +1,18 @@
 package com.example.alumno.vwmove;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class RouteActivity extends AppCompatActivity {
+
+    Button profile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +22,12 @@ public class RouteActivity extends AppCompatActivity {
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemSelectedListener(listener);
 
+        profile = (Button)findViewById(R.id.toolbar_profile);
+    }
 
-
+    public void go_profile(View v){
+        Intent prof = new Intent(this, ProfileActivity.class);
+        startActivity(prof);
     }
 
     BottomNavigationView.OnNavigationItemSelectedListener listener =
